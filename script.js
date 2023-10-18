6f10ca31cf23242df3072904fcc7f19bb90d4161
Shashank Naik <shashanknaik808@gmail.com>
2023-10-18 16:16:20 +0530

    Getting All the Required Elements

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const selectBox = document.querySelector(".select-box"),
     selectBtnX = selectBox.querySelector(".options .playerX"),
-    selectBtnO = selectBox.querySelector(".options .playerO");
+    selectBtnO = selectBox.querySelector(".options .playerO"),
+    playBoard = document.querySelector(".play-board"),
+    players = document.querySelector(".players"),
+    allBox = document.querySelectorAll("section span"),
+    resultBox = document.querySelector(".result-box"),
+    wonText = resultBox.querySelector(".won-text"),
+    replayBtn = resultBox.querySelector("button");
 
 
 window.onload = () => {
