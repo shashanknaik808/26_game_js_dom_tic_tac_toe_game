e95b45edb132313486287acd0ed737d63d5e38d5
Shashank Naik <shashanknaik808@gmail.com>
2023-10-18 16:03:57 +0530

    Hiding selection box and showing playboard section on Player (X)  button click

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const selectBox = document.querySelector(".select-box"),
     selectBtnX = selectBox.querySelector(".options .playerX"),
     selectBtnO = selectBox.querySelector(".options .playerO");
+
+
+window.onload = () => {
+    for (let i = 0; i < allBox.length; i++) {
+        allBox[i].setAttribute("onclick", "clickedBox(this)");
+    }
+}
+
+selectBtnX.onclick = () => {
+    selectBox.classList.add("hide");
+    playBoard.classList.add("show");
+}
