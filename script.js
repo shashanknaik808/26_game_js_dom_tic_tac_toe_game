b12c9ab572555a6e703d1bfa60278d9dc92a9fa9
Shashank Naik <shashanknaik808@gmail.com>
2023-10-18 16:08:21 +0530

    Implemented player click handling and board update logic

--- script.js
+++ script.js
@@ -19,3 +19,25 @@ selectBtnO.onclick = () => {
     playBoard.classList.add("show");
     players.setAttribute("class", "players active player");
 }
+
+
+let playerXIcon = "fas fa-times",
+    playerOIcon = "far fa-circle",
+    playerSign = "X",
+    runBot = true;
+
+function clickedBox(element) {
+    if (players.classList.contains("player")) {
+        playerSign = "O";
+        element.innerHTML = `<i class="${playerOIcon}"></i>`;
+        players.classList.remove("active");
+        element.setAttribute("id", playerSign);
+    } else {
+        element.innerHTML = `<i class="${playerXIcon}"></i>`;
+        element.setAttribute("id", playerSign);
+        players.classList.add("active");
+    }
+    selectWinner();
+    element.style.pointerEvents = "none";
+    playBoard.style.pointerEvents = "none";
+}
