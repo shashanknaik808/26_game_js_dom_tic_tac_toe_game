af95dd7a2398fcd7965241b754f9082ba2f4cf1c
Shashank Naik <shashanknaik808@gmail.com>
2023-10-18 15:59:17 +0530

    Selecting the Required Elements

---
+++ script.js
@@ -0,0 +1,3 @@
+const selectBox = document.querySelector(".select-box"),
+    selectBtnX = selectBox.querySelector(".options .playerX"),
+    selectBtnO = selectBox.querySelector(".options .playerO");
